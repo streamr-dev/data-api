6a1095fcbfd1ff839caac3fdb945d4e8fec07122
Eric Andrews <eric.andrews@streamr.com>
2015-09-28 16:35:09 +0300

    CORE-396: multiple performance clients

--- performance-test/run.js
+++ performance-test/run.js
@@ -1,6 +1,7 @@
 global.io = require("socket.io-client")
 var StreamrClient = require("./lib/streamr-client/streamr-client.js").StreamrClient
 
+
 var serverUrl = "http://dev.unifina:8889"
 var streamId = "1ef8TbyGTFiAlZ8R2gCaJw"
 
@@ -27,12 +28,10 @@ function PerformanceTestClient() {
 
 			function (message, streamId, counter) {
 				++that.numOfMessagesReceived
-				console.log("*")
+				//console.log("*")
 			},
 
-			{
-				// Resend options
-			}
+			{}
 	)
 }
 
@@ -40,20 +39,39 @@ PerformanceTestClient.prototype.start = function() {
 	this.client.connect()
 }
 
-PerformanceTestClient.prototype.printStatus = function() {
-	console.log({
+
+PerformanceTestClient.prototype.state = function() {
+	return {
 		didConnect: this.isConnected,
 		didSubscribe: this.client.subsByStream[streamId][0].subscribed,
 		numOfMessagesReceived: this.numOfMessagesReceived
-	})
+	}
 }
 
+var clients = []
 
-var client = new PerformanceTestClient()
-client.start()
+for (var i=0; i < 300; ++i) {
+	var client = new PerformanceTestClient()
+	client.start()
+	clients.push(client)
+}
 
 process.on("SIGINT", function() {
-	client.printStatus()
+	var numOfConnects = 0
+	var numOfSubscribes = 0
+	var numOfMessagesReceivedPerClient = []
+
+	clients.map(function(client) {
+		return client.state()
+	}).map(function(state) {
+		numOfConnects += state.didConnect
+		numOfSubscribes += state.didSubscribe
+		numOfMessagesReceivedPerClient.push(state.numOfMessagesReceived)
+	})
+
+	console.log("Number of connects " + numOfConnects)
+	console.log("Number of subscribes " + numOfSubscribes)
+	console.log("Numbers of messages received " + numOfMessagesReceivedPerClient)
 	process.exit()
 })
 
