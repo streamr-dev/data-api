ccadf1dfa269166e8517eae172997997abd63be4
Eric Andrews <eric.andrews@streamr.com>
2015-09-29 15:05:00 +0300

    CORE-396 lower constants

--- performance-test/constants.js
+++ performance-test/constants.js
@@ -10,8 +10,8 @@ define("SERVER_PORT", 8890);
 define("STREAM_ID", "testStreamId");
  define("SERVER_URL", "http://dev.unifina:8890");
 // define("STREAM_ID", "1ef8TbyGTFiAlZ8R2gCaJw"); // Front page demo
-define("NUM_OF_MESSAGES_TO_SEND", 5000)
+define("NUM_OF_MESSAGES_TO_SEND", 2500)
 define("MESSAGE_RATE_IN_MILLIS", 200)
-define("TOTAL_CLIENTS", 250)
+define("TOTAL_CLIENTS", 100)
 define("CLIENT_RAMPUP_IN_MILLIS", 5)
 
