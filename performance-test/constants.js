9522a4f307b07a6c6a0161178bff73033482e422
Eric Andrews <eric.andrews@streamr.com>
2015-09-29 10:02:50 +0000

    CORE-369 dev.unifina settings

--- performance-test/constants.js
+++ performance-test/constants.js
@@ -5,13 +5,13 @@ function define(name, value) {
 	});
 }
 
-define("SERVER_PORT", 8889);
-define("SERVER_URL", "http://localhost:8889");
+define("SERVER_PORT", 8890);
+//define("SERVER_URL", "http://localhost:8889");
 define("STREAM_ID", "testStreamId");
-// define("SERVER_URL", "http://dev.unifina:8889");
+ define("SERVER_URL", "http://dev.unifina:8890");
 // define("STREAM_ID", "1ef8TbyGTFiAlZ8R2gCaJw"); // Front page demo
-define("NUM_OF_MESSAGES_TO_SEND", 100)
-define("MESSAGE_RATE_IN_MILLIS", 10)
-define("TOTAL_CLIENTS", 10)
+define("NUM_OF_MESSAGES_TO_SEND", 5000)
+define("MESSAGE_RATE_IN_MILLIS", 200)
+define("TOTAL_CLIENTS", 250)
 define("CLIENT_RAMPUP_IN_MILLIS", 5)
 
