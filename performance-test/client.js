e0428b62d3a1923aa06fe0ddf76d41507db74f9b
Eric Andrews <eric.andrews@streamr.com>
2015-09-29 15:00:08 +0300

    COREE-396 mean of max and min latencies per client

--- performance-test/client.js
+++ performance-test/client.js
@@ -62,12 +62,18 @@ function createAndConnectClient() {
 	}
 }
 
+function meanOfArray(list) {
+	return list.reduce(function(sum, val) { return sum + val }, 0) / list.length
+}
+
 // When process is killed, print out stats
 process.on("SIGINT", function() {
 	var numOfConnects = 0
 	var numOfSubscribes = 0
 	var numOfMessagesReceivedPerClient = []
 	var meanLatencies = []
+	var minLatencies = []
+	var maxLatencies = []
 	var maxLatency = Number.NEGATIVE_INFINITY
 	var minLatency = Number.POSITIVE_INFINITY
 
@@ -76,20 +82,26 @@ process.on("SIGINT", function() {
 		numOfSubscribes += client.isSubscribed()
 		numOfMessagesReceivedPerClient.push(client.numOfMessagesReceived)
 		meanLatencies.push(client.sumOfTimeDiffs / client.numOfMessagesReceived)
+		minLatencies.push(client.minTimeDiff)
+		maxLatencies.push(client.maxTimeDiff)
 		maxLatency = Math.max(maxLatency, client.maxTimeDiff)
 		minLatency = Math.min(minLatency, client.minTimeDiff)
 	})
 
 	// Mean of means not a problem because subsamples should be of equal size
-	var grandMean = meanLatencies.reduce(function(sum, val) {
-		return sum + val
-	}, 0) / meanLatencies.length
+	var grandMean = meanOfArray(meanLatencies)
+	var minMean = meanOfArray(minLatencies)
+	var maxMean = meanOfArray(maxLatencies)
 
 	console.log("Number of connects " + numOfConnects)
 	console.log("Number of subscribes " + numOfSubscribes)
 	console.log("Numbers of messages received " + numOfMessagesReceivedPerClient)
-	console.log("Latency interval [" + [minLatency, maxLatency] + "] ms")
-	console.log("Mean latency " + grandMean  + " ms")
+	console.log("")
+	console.log("---- Latency ----")
+	//console.log("Mean latencies" + meanLatencies)
+	console.log("Interval [" + [minLatency, maxLatency] + "] ms")
+	console.log("Mean " + grandMean  + " ms")
+	console.log("Mean min and max [" + [minMean, maxMean] + "] ms")
 	process.exit()
 })
 
