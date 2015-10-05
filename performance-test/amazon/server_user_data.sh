57811a4a12982e61f155d604dc0afd740444b73b
Eric Andrews <eric.andrews@streamr.com>
2015-10-05 21:13:20 +0300

    CORE-396 file limits really into effect

--- performance-test/amazon/server_user_data.sh
+++ performance-test/amazon/server_user_data.sh
@@ -1,5 +1,7 @@
 su ubuntu <<'EOF'
 sudo rdate -s tick.greyware.com
 screen -S 'server' -d -m nodejs /home/ubuntu/streamr-socketio-server/performance-test/server.js
+sleep 2
+sudo prlimit --pid $(pgrep node) --nofile=32000:50000
 EOF
 
