cc17f8650e40944d657dab86b25bcd13892678cd
Eric Andrews <eric.andrews@streamr.com>
2015-10-09 15:14:09 +0300

    CORE-396 update configuration settings

--- performance-test/constants.js
+++ performance-test/constants.js
@@ -6,17 +6,17 @@ function define(name, value) {
 }
 
 // amazon.js
-define("NUM_OF_EC2_INSTANCES", 10)
+define("NUM_OF_EC2_INSTANCES", 21)
 
 // client.js
-define("NUM_OF_CLIENTS_PER_INSTANCE", 200)
+define("NUM_OF_CLIENTS_PER_INSTANCE", 1000)
 define("SERVER_URL", "http://localhost:8890");
-define("CLIENT_RAMPUP_IN_MILLIS", 100)
+define("CLIENT_RAMPUP_IN_MILLIS", 200)
 define("LATENCY_LOG_FILE", "latencies.csv")
 
 // server.js
-define("NUM_OF_MESSAGES_TO_SEND", 1500)
-define("MESSAGE_RATE_IN_MILLIS", 500)
+define("NUM_OF_MESSAGES_TO_SEND", 1000)
+define("MESSAGE_RATE_IN_MILLIS", 1000)
 define("TOTAL_CLIENTS",
 		(this.NUM_OF_EC2_INSTANCES - 1) * this.NUM_OF_CLIENTS_PER_INSTANCE)
 
