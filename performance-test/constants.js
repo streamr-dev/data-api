c0e2a020321fe500a161033ef8c449b585837b13
Eric Andrews <eric.andrews@streamr.com>
2015-10-12 17:18:32 +0300

    CORE-396 re-order constants

--- performance-test/constants.js
+++ performance-test/constants.js
@@ -5,14 +5,21 @@ function define(name, value) {
 	});
 }
 
+// amazon.js
 define("NUM_OF_EC2_INSTANCES", 2)
+
+// client.js
 define("NUM_OF_CLIENTS_PER_INSTANCE", 1)
-define("KAFKA_URL", "dev.unifina:2181")
 define("SERVER_URL", "http://localhost:8890")
 define("CLIENT_RAMPUP_IN_MILLIS", 15)
+
+// server.js / server-with-kafka.js / data-generator.js
+define("KAFKA_URL", "dev.unifina:2181")
 define("TOTAL_CLIENTS",
 		(this.NUM_OF_EC2_INSTANCES - 1) * this.NUM_OF_CLIENTS_PER_INSTANCE)
 define("MESSAGE_RATE_IN_MILLIS", 15000)
+
+// shared
 define("LATENCY_LOG_FILE", "latencies.csv")
 define("NUM_OF_MESSAGES", 1000)
 define("SERVER_PORT", 8890);
